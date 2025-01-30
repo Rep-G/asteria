fb06d74a2441cac3129c2ddbf43406fc96266832
Aryan Mahalingham <aryan.maha202@gmail.com>
2025-01-29 20:49:41 -0500

    stuf

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Rep-G/asteria
+
+go 1.22.11
---
+++ internal/services/redis.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"context"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var RedisClient *redis.Client
+
+func InitRedis() {
+	RedisClient = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379", // Change if needed
+	})
+	_, err := RedisClient.Ping(context.Background()).Result()
+	if err != nil {
+		log.Fatalf("Failed to connect to Redis: %v", err)
+	}
+	log.Println("Connected to Redis")
+}
