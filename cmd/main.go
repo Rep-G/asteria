5d7a71d0269447a10204d810288ba0180d7627c2
Aryan Mahalingham <aryan.maha202@gmail.com>
2025-01-29 20:54:23 -0500

    tuffy will start tmrw

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Rep-G/asteria/internal/api"
+	"github.com/Rep-G/asteria/internal/database"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	database.InitRedis()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/fortnite/api/game/v2/profile/{accountId}/client/QueryProfile", api.QueryProfileHandler).Methods("POST")
+	r.HandleFunc("/fortnite/api/game/v2/matchmaking/account/{accountId}", api.MatchmakingHandler).Methods("GET")
+
+	fmt.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
--- internal/services/redis.go
+++ internal/services/redis.go
@@ -11,7 +11,7 @@ var RedisClient *redis.Client
 
 func InitRedis() {
 	RedisClient = redis.NewClient(&redis.Options{
-		Addr: "localhost:6379", // Change if needed
+		Addr: "localhost:6379",
 	})
 	_, err := RedisClient.Ping(context.Background()).Result()
 	if err != nil {
